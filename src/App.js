335355dcc3efeeb10614924802cff43a223109d1
Gustavo Decker Zambrano <gustavdz@gmail.com>
2020-05-27 18:48:30 -0500

    envio a backend

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import EmailEditor from "react-email-editor";
 import sample from './sample2.json'
+import axios from "axios";
 
 class App extends Component {
   render() {
@@ -8,6 +9,7 @@ class App extends Component {
       <div>
         <button onClick={this.saveDesign}>Save Design</button>
         <button onClick={this.exportHtml}>Export HTML</button>
+        <button onClick={this.sendEmail}>Send Email</button>
       </div>
 
       <EmailEditor
@@ -17,13 +19,25 @@ class App extends Component {
           options={{ locale: 'es-ES' }}
       />
     </div>
-
   }
-  exportHtml = () => {
+  exportHtml = async () => {
     this.editor.exportHtml(data => {
       const { design, html } = data
       console.log('exportHtml', html)
     })
+  }
+  sendEmail= async () => {
+    await this.editor.exportHtml(async data => {
+      const { html } = data
+
+      const response = await axios.post('http://localhost:4000/send-email', {mensaje: html},{
+        headers: {'Content-Type': 'application/json', 'Accept': 'application/json'}
+      }).catch((error)=>{
+        console.log(error);
+      });
+      console.log(response);
+    })
+
   }
   saveDesign = () => {
     this.editor.saveDesign(design => {
@@ -36,7 +50,6 @@ class App extends Component {
     // this.editor.addEventListener('onDesignLoad', this.onDesignLoad)
     this.editor.loadDesign(sample)
   }
-
   onDesignLoad = (data) => {
     console.log('onDesignLoad', data)
   }
