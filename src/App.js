d6cc537bf26e67d63a985af8897de57949dbd0c7
Gustavo Decker Zambrano <gustavdz@gmail.com>
2020-05-27 00:07:36 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import EmailEditor from "react-email-editor";
+import sample from './sample2.json'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component {
+  render() {
+    return <div>
+      <div>
+        <button onClick={this.saveDesign}>Save Design</button>
+        <button onClick={this.exportHtml}>Export HTML</button>
+      </div>
+
+      <EmailEditor
+          ref={editor => this.editor = editor}
+          onLoad={this.onLoad}
+          onDesignLoad={this.onDesignLoad}
+          options={{ locale: 'es-ES' }}
+      />
     </div>
-  );
+
+  }
+  exportHtml = () => {
+    this.editor.exportHtml(data => {
+      const { design, html } = data
+      console.log('exportHtml', html)
+    })
+  }
+  saveDesign = () => {
+    this.editor.saveDesign(design => {
+      //console.log('saveDesign', design)
+      console.log('saveDesign', JSON.stringify(design))
+      alert("Design JSON has been logged in your developer console.")
+    })
+  }
+  onLoad = () => {
+    // this.editor.addEventListener('onDesignLoad', this.onDesignLoad)
+    this.editor.loadDesign(sample)
+  }
+
+  onDesignLoad = (data) => {
+    console.log('onDesignLoad', data)
+  }
 }
 
 export default App;
+
